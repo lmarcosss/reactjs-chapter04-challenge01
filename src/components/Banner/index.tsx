ec47e3f3e3c6de185a3866fe0e693da57ccbecf2
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2021-08-26 16:33:31 -0300

    adjust banner resposible layout

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -15,20 +15,22 @@ export function Banner() {
       align="center"
       justify="space-around"
     >
-      <Flex
-        direction="column"
-        alignItems={isWideVersion ? "center" : "flex-start"}
-      >
+      <Flex direction="column" alignItems={"flex-start"}>
         <Text fontSize={["20", "30", "36"]} fontWeight="500" color="white">
           5 Continentes,
         </Text>
         <Text fontSize={["20", "30", "36"]} fontWeight="500" color="white">
           infinitas possibilidades.
         </Text>
-        <Text fontSize={["14", "15", "20"]} fontWeight="400" color="white">
+        <Text
+          pt={["2", "4"]}
+          fontSize={["14", "15", "20"]}
+          fontWeight="400"
+          color="gray.300"
+        >
           Chegou a hora de tirar do papel a viagem que você
         </Text>
-        <Text fontSize={["14", "15", "20"]} fontWeight="400" color="white">
+        <Text fontSize={["14", "15", "20"]} fontWeight="400" color="gray.300">
           sempre sonhou.
         </Text>
       </Flex>
